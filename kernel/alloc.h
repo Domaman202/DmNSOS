61ee8ce042f4230397231639cf8f079dbd3a9b1e
Domaman202 <vip.domaman@mail.ru>
2021-12-11 17:04:46 +0700

    XD
    теперь эта хуйня должна быть более человечной чем старая наша и эта чужая версия

--- kernel/alloc.h
+++ kernel/alloc.h
@@ -3,64 +3,46 @@
 
 #include "stdlib.h"
 
-typedef struct mem_block_header {
-    size_t size;
-    int free;
-    struct mem_block_header *next;
-    struct mem_block_header *prev;
-} mem_block_header;
+typedef struct mem_block {
+    uint8_t flags;
+    struct mem_block* next;
+} mem_block;
 
-mem_block_header* mem_start_block;
-
-void mem_init() {
-    mem_start_block = (mem_block_header*) MEM_SPACE;
-    mem_start_block->free = 1;//if free 1,if allocated 0
-    mem_start_block->size = 25000 - sizeof(mem_block_header);
-    mem_start_block->prev = 0;
-    mem_start_block->next = 0;
+void mem_init(void) {
+    mem_block* block = (mem_block*) MEM_START;
+    block->flags = 0x0;
+    block->next = MEM_END;
 }
 
-mem_block_header *newdata(mem_block_header *block, size_t size, size_t freesize) {
-    mem_block_header *newdata = (mem_block_header *)((char *)block + size + sizeof(mem_block_header) + 1);
-    newdata->size = freesize - size - sizeof(mem_block_header) - 1;
-    newdata->free = 1;
-    newdata->next = block->next;
-    newdata->prev = block;
-    return newdata;
+size_t mb_size(mem_block* mem_block) {
+    return mem_block->next - mem_block;
+}
+
+mem_block* split_heap(size_t size) {
+    mem_block* last_block = (mem_block*) MEM_START;
+    while (last_block->flags == 0x1)
+        last_block = last_block->next;
+
+    if (mb_size(last_block) == size) {
+        last_block->flags = 0x1;
+        return last_block;
+    }
+
+    last_block->flags = 0x1;
+
+    mem_block* new_next_block = (mem_block*)((long) last_block + (long) size + (long) sizeof(mem_block));
+    new_next_block->next = last_block->next;
+    last_block->next = new_next_block;
+
+    return last_block;
 }
 
 void* malloc(size_t size) {
-    mem_block_header *travel = mem_start_block;
-    while (1) {
-        if (travel == 0)
-            return 0;
-        else if (travel->size >= (size + sizeof(mem_block_header)) && travel->free == 1) {
-            travel->next = newdata(travel, size, travel->size);
-            travel->size = size;
-            travel->free = 0;
-            break;
-        }
-        travel = travel->next;
-    }
-    return (char *) travel + sizeof(mem_block_header) + 1;
+    return split_heap(size) + sizeof(mem_block);
 }
 
-void free(void *ptr) {
-    mem_block_header *info = (mem_block_header *) ((char *) ptr - 1) - 1;
-    info->free = 1;
-    if (info->next != 0 && info->next->free == 1) {
-        info->size += info->next->size + sizeof(mem_block_header) + 1;
-        if (info->next->next != 0)
-            info->next->next->prev = info;
-        info->next = info->next->next;
-    }
-
-    if (info->prev != 0 && info->prev->free == 1) {
-        info->prev->size += info->size + sizeof(mem_block_header) + 1;
-        if (info->next != 0)
-            info->next->prev = info->prev;
-        info->prev->next = info->next;
-    }
+void free(void* ptr) {
+    ((mem_block*) ((long) ptr - (long) sizeof(mem_block)))->flags = 0x0;
 }
 
 #endif //ALLOC_H
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,7 +11,21 @@ void kmain(void) {
     mem_init();
     println_string("Memory initialized!");
     //
-
+    void* ptr0 = malloc(256);
+    void* ptr1 = malloc(256);
+    void* ptr2 = malloc(256);
+    void* ptr3 = malloc(256);
+    //
+    char line[80];
+    println_string(itoa(line, ptr0));
+    println_string(itoa(line, ptr1));
+    println_string(itoa(line, ptr2));
+    println_string(itoa(line, ptr3));
+    //
+    free(ptr0);
+    free(ptr1);
+    free(ptr2);
+    free(ptr3);
     //
     while (1);
 }
