7ce04704275bf0b6d1a7265929cfc32fade5bf5e
Domaman202 <vip.domaman@mail.ru>
2021-12-15 08:54:30 +0700

    Update kernel.cpp

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -11,16 +11,5 @@ extern "C" void kmain(void) {
     mem_init();
     println_string("Memory initialized!");
     //
-    char line[80];
-    //
-    void *ptr4 = malloc(256);
-    println_string(itoa(line, mb_size((mem_block*) ((uintptr_t) ptr4 - sizeof(mem_block)))));
-    ptr4 = realloc(ptr4, 128);
-    println_string(itoa(line, mb_size((mem_block*) ((uintptr_t) ptr4 - sizeof(mem_block)))));
-    ptr4 = realloc(ptr4, 256);
-    println_string(itoa(line, mb_size((mem_block*) ((uintptr_t) ptr4 - sizeof(mem_block)))));
-    ptr4 = realloc(ptr4, 512);
-    println_string(itoa(line, mb_size((mem_block*) ((uintptr_t) ptr4 - sizeof(mem_block)))));
-    //
     while (1);
 }
