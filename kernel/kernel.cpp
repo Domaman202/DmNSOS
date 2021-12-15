ca4cf4844c1a701d48678f7fe659174e1e93003c
Domaman202 <vip.domaman@mail.ru>
2021-12-15 13:43:46 +0700

    Update kernel.cpp

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -2,19 +2,19 @@
 
 namespace DmNSOS {
     extern "C" void kmain(void) {
-            //
-            init_vga();
-            println_string("VGA initialized!");
-            //
-            idt_init();
-            println_string("IDT initialized!");
-            //
-            mem_init();
-            println_string("Memory initialized!");
-            //
+        //
+        init_vga();
+        println_string("VGA initialized!");
+        //
+        idt_init();
+        println_string("IDT initialized!");
+        //
+        mem_init();
+        println_string("Memory initialized!");
+        //
 //        fs_init();
-            println_string("TODO: FileSystem!");
-            //
-            while (1);
+        println_string("TODO: FileSystem!");
+        //
+        while (1);
     }
 }
