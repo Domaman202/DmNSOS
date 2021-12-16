0557712eec1390e71af15453909854a8d7214a9c
Domaman202 <vip.domaman@mail.ru>
2021-12-16 21:45:02 +0700

    start move to C++

--- kernel/include/screen.h
+++ kernel/include/screen.h
@@ -2,6 +2,7 @@
 #define __SCREEN_H_
 
 #include "stdlib.h"
+#include "stddef.h"
 
 #define VGA_SIZE vga_w * vga_h
 
@@ -10,6 +11,7 @@ extern uint8_t vga_w;
 extern uint8_t vga_h;
 extern uint16_t vga_x, vga_y;
 
+EXTERN_C_START
 uint16_t vga_entry(unsigned char ch);
 void clear_vga_buffer(void);
 void init_vga(void);
@@ -18,5 +20,6 @@ void println(void);
 void print_char(char ch);
 void print_string(char *str);
 void println_string(char *str);
+EXTERN_C_END
 
 #endif //__SCREEN_H_
