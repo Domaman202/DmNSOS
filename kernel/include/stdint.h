137e7267bfbcabac886c8bc6834795d986a13f22
Domaman202 <vip.domaman@mail.ru>
2021-12-20 06:57:48 +0700

    Update stdint.h

--- kernel/include/stdint.h
+++ kernel/include/stdint.h
@@ -1,7 +1,7 @@
 #ifndef __STDINT_H_
 #define __STDINT_H_
 
-typedef char int8_t;
+typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef short int16_t;
 typedef unsigned short uint16_t;
