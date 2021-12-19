75456626033e7b126e0bb954b092c7bed65b7075
Domaman202 <vip.domaman@mail.ru>
2021-12-19 23:53:42 +0700

    Revert "Update kernel.cpp"
    This reverts commit 5aa24a166e9fffa0a5f6ba7af0736718a9afa695.

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,25 +1,36 @@
 #include "include/kernel.h"
 
-namespace DmNSOS {
-    extern "C" void kmain() {
-        init_vga();
-        idt_init();
-        mem_init();
+void fs_init() {
 
-        println_string("DmNSOS");
-        println_string("Version 0.1");
-        while (1) {
-            print_string("> ");
-            char *line = readline();
+}
 
-            if (strcmp(line, "exit") == 0) {
-                return;
-            } else if (strcmp(line, "hello") == 0) {
-                println_string("Hello!");
-            } else {
-                print_string(line);
-                println_string(": not found");
-            }
+void init() {
+    init_vga();
+    idt_init();
+    mem_init();
+}
+
+extern "C" void kmain() {
+    init();
+
+    println_string("    __ _                   ___  __     ");
+    println_string("   / /(_)_ __ ___   __ _  /___\\/ _\\  ");
+    println_string("  / / | | '_ ` _ \\ / _` |//  //\\ \\  ");
+    println_string(" / /__| | | | | | | (_| / \\_// _\\ \\ ");
+    println_string(" \\____/_|_| |_| |_|\\__,_\\___/  \\__/");
+    println();
+    println_string(" Version 0.1 - Alpha. Based on DmNSOS");
+    while (1) {
+        print_string("> ");
+        char *line = readline();
+
+        if (strcmp(line, "exit") == 0) {
+            return;
+        } else if (strcmp(line, "hello") == 0) {
+            println_string("Hello!");
+        } else {
+            print_string(line);
+            println_string(": not found");
         }
     }
 }
