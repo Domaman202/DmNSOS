f793b7a5fa065019782dd2be144e65270ddb674d
Domaman202 <vip.domaman@mail.ru>
2021-12-20 19:23:07 +0700

    Update kernel.cpp

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -19,11 +19,11 @@ namespace DmNSOS {
             char *line = readline();
             vga_nln();
 
-            if (strcmp(line, "exit") == 0) {
+            if (strcmp(line, "exit") == 0)
                 return;
-            } else if (strcmp(line, "hello") == 0) {
+            else if (strcmp(line, "hello") == 0)
                 println_string("Hello!");
-            } else {
+            else {
                 print_string(line);
                 println_string(": not found");
             }
